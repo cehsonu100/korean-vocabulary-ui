31a4e0ff6c67a35911fc6d4dad60fb11c6f621c3
Sonu Kumar <cehsonu100@gmail.com>
2020-09-29 20:04:21 +0530

    Very first structure

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export interface LayoutProps {
+  
+}
+export interface LayoutState {
+  
+}
+
+export class Layout extends React.Component<LayoutProps,LayoutProps> {
+  constructor(props: LayoutProps) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <h1>Hello there</h1>
+    );
+  }
+}
